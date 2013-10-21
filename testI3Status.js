4a439239bcaf1aa4bd203cc1be5d6ba4d5a7aff9
Jason Kridner <jdk@ti.com>
2013-10-21 11:16:54 -0400

    Add an app to test for the i3 Detroit space status

---
+++ testI3Status.js
@@ -0,0 +1,41 @@
+var http = require('http');
+
+doRequest();
+setInterval(doRequest, 60000);
+
+var previousSpaceStatus = "";
+function doRequest() {
+  var req = http.get({hostname:'www.i3detroit.org'}, i3Request);
+  req.on('error', function(e) {
+    console.log('Problem with request: ' + e.message);
+  });
+
+  var page = "";
+  function i3Request(res) {
+    if(res.statusCode != 200) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      return;
+    }
+    res.setEncoding('utf8');
+    res.on('data', onData);
+    res.on('end', onEnd);
+  
+    function onData(chunk) {
+      page += chunk;
+      //console.log('BODY: ' + chunk);
+    }
+  
+    function onEnd() {
+      var space = page.match(/The space is currently.*\n<span.*>(.*)<\/span>/mi);
+      if(space.length != 2) {
+        console.log("BODY doesn't contain space status: " + page);
+        return;
+      }
+      if(space[1] != previousSpaceStatus) {
+        console.log('SPACE: ' + space[1]);
+        previousSpaceStatus = space[1];
+      }
+    }
+  }
+}
